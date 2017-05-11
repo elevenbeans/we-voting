64b09018f5cc945b86a7c72ef29d2181b353bdf4
elevenBeans <elevenbeansf2e@gmail.com>
2017-05-11 19:29:20 +0800

    disable landing without login

--- client/js/new.jsx
+++ client/js/new.jsx
@@ -10,10 +10,15 @@ class New extends Component {
 	constructor(props) {
     super(props);
   }
+  componentDidMount() {
+    if(!appInfo.name){
+       location.href = location.origin + '/login/github'; // 不支持 landing! 滚去登陆
+    }
+  }
   render() {
     return (
 			<div className = "newpage">
-				new page~
+				new page ~
 				<Footer />
 			</div>
     );
