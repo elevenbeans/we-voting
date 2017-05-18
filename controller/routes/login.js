0d6f17dabe3c6019a554d04d528c739a22632651
Emily Hee <emilyhee@EmilydeMacBook-Air.local>
2017-05-19 04:09:25 +0800

    bug fix

--- controller/routes/login.js
+++ controller/routes/login.js
@@ -53,11 +53,11 @@ router.get('/github/callback', function(req, resp){
 
 					dbhandler.insertUser(data,
 						function(data){
-		 					resp.redirect(‘/‘);
+		 					resp.redirect('/');
 						},
 						function(err){
 							if(err === 'EXIST_USER') {
-								resp.redirect(‘/‘);
+								resp.redirect('/');
 							} else {
 								resp.send(err);
 							}
